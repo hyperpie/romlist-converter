5fc3ca581a01ecfe2bd20c9636f8151da16a519b
Jonas Lundblad <jolnyx@gmail.com>
2018-01-24 23:22:51 +0100

    quotes for

--- dialog.sh
+++ dialog.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-listfolder='/home/pi/.attract/romlists/'
+listfolder='/home/jonas/romlists/'
+#'/home/pi/.attract/romlists/'
 
 menu=$(dialog --stdout --menu "Convert which romlists?" 10 30 4 \
     1 "Convert all" \
@@ -13,10 +14,11 @@ cmd=(dialog --stdout --no-items \
 if (($menu == 1))
 then
 files=$(find $listfolder -maxdepth 1 -regex '\(.*lassics.*\|.*ollection.*\).*.txt' -printf '%f\n')
-python RomlistConverter.py $files
+python test.py "$files"
 elif (($menu == 2))
 then
-options=$(find $listfolder -maxdepth 1 -regex '\(.*lassics.*\|.*ollection.*\).*.txt' -printf '%f\n' | awk '{print $"1", "off"}')
+options=$(find $listfolder -maxdepth 1 -regex '\(.*lassics.*\|.*ollection.*\).*.txt' -printf "'%f'\n" | awk '{print $0, "off"}')
+
 choices=$("${cmd[@]}" ${options})
-python RomlistConverter.py $choices
+python test.py "$choices"
 fi
