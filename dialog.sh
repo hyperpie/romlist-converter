76b28398a95e2840fcbd84c22a937c350ef2c56b
Jonas Lundblad <jolnyx@gmail.com>
2018-01-25 21:47:58 +0100

    working now? sed replace for names with spaces

--- dialog.sh
+++ dialog.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-listfolder='/home/jonas/romlists/'
-#'/home/pi/.attract/romlists/'
+listfolder='/home/pi/.attract/romlists/'
 
-menu=$(dialog --stdout --menu "Convert which romlists?" 10 30 4 \
-    1 "Convert all" \
-    2 "Select lists manually")
+regex='\(.*cave.*\|.*pgm.*\|.*psikyo.*\|.*technos.*\|.*visco.*\|.*wwf.*\|.*classics.*\|.*collection.*\|.*hacks.*\|.*shmup.*\|.*hacks.*\).*.txt'
+
+menu=$(dialog --stdout --menu "Convert which romlists?" 10 50 20 \
+    1 "Convert all collections" \
+    2 "Select manually from collections" \
+    3 "Select from all rom lists")
 
 cmd=(dialog --stdout --no-items \
         --separate-output \
         --ok-label "Convert" \
-        --checklist "Select romlists to convert:" 22 76 16)
+        --checklist "Select romlists to convert (SPACE selects):" 22 76 16)
 
 if (($menu == 1))
 then
-files=$(find $listfolder -maxdepth 1 -regex '\(.*lassics.*\|.*ollection.*\).*.txt' -printf '%f\n')
-python test.py "$files"
+files=$(find $listfolder -maxdepth 1 -iregex $regex -printf '%f\n')
+python RomlistConverter.py "$files"
 elif (($menu == 2))
 then
-options=$(find $listfolder -maxdepth 1 -regex '\(.*lassics.*\|.*ollection.*\).*.txt' -printf "'%f'\n" | awk '{print $0, "off"}')
+options=$(find $listfolder -maxdepth 1 -iregex $regex -printf "%f\n" | sed 's/ /+/g' | awk '{print $0, "off"}')
 
-choices=$("${cmd[@]}" ${options})
-python test.py "$choices"
+choices=$("${cmd[@]}" ${options} | sed 's/+/ /g')
+python RomlistConverter.py "$choices"
+elif (($menu == 3))
+then
+options=$(find $listfolder -maxdepth 1 -iregex '.*.txt' -printf "%f\n" | sed 's/ /+/g' | awk '{print $0, "off"}')
+
+choices=$("${cmd[@]}" ${options} | sed 's/+/ /g')
+python RomlistConverter.py "$choices"
 fi
